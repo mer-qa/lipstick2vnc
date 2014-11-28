82fa1d536f6a35a42efe57555cf8ca29d735c4a5
Reto Zingg <reto.zingg@jolla.com>
2014-11-28 17:10:28 +0200

    add copyright/gpl2 header

--- src/buffer.cpp
+++ src/buffer.cpp
@@ -1,3 +1,27 @@
+/****************************************************************************
+ *
+ *  buffer.cpp - Mervncserver, a VNC server for Mer devices
+ *  Copyright (C) 2014 Jolla Ltd.
+ *  Contact: Reto Zingg <reto.zingg@jolla.com>
+ *
+ *  This file is part of Mervncserver.
+ *
+ *  Mervncserver is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License along
+ *  with this program; if not, write to the Free Software Foundation, Inc.,
+ *  51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ *
+ ****************************************************************************/
+
 #include "buffer.h"
 
 Buffer *Buffer::create(wl_shm *shm, QScreen *screen)
